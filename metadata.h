4f3e7b3618d94d5e85099c256140a76aef9bae0c
ZhenDu <454858191@qq.com>
2018-01-07 15:20:48 +0800

    2018-01-07

--- metadata.h
+++ metadata.h
@@ -17,28 +17,24 @@ struct __metadata_entry {
 };
 typedef struct __metadata_entry metadata_entry_t;
 
-printf("1");
-
 //一个指向50位的元数据表指针的指针
 metadata_entry_t** meta_table_point = NULL;
-
-printf("2");
 
 //元数据表的初始化函数
 void init_metadata_table(metadata_entry_t **input_meta_table){
     //申请一段空间，来存储元数据
     //用一个临时指针来申请空间
     printf("准备初始化元数据表");
-    metadata_entry_t* tmp_point = 
-        (metadata_entry_t*)malloc(sizeof(metadata_entry_t)*META_TABLE_SIZE);
+    // metadata_entry_t* tmp_point = 
+    //     (metadata_entry_t*)malloc(sizeof(metadata_entry_t)*META_TABLE_SIZE);
 
     //初始化所有的空间
     printf("正在初始化分配的空间");
-    memset(tmp_point, 0, sizeof(metadata_entry_t)*META_TABLE_SIZE);
+    // memset(tmp_point, 0, sizeof(metadata_entry_t)*META_TABLE_SIZE);
 
     //判断一个这个条目是不是有东西，主要就是判断两个指针是不是都是0。当两个指针是0的时候
     //就代表这个条目是空的
-    *input_meta_table = tmp_point;
+    // *input_meta_table = tmp_point;
     printf("元数据表初始化完毕");
 }
 
