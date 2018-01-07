3ab772fb6ee8f9f82141223e286bd6ba3ce7375e
ZhenDu <454858191@qq.com>
2018-01-07 15:18:41 +0800

    2018-01-07

--- metadata.h
+++ metadata.h
@@ -17,8 +17,12 @@ struct __metadata_entry {
 };
 typedef struct __metadata_entry metadata_entry_t;
 
+printf("1");
+
 //一个指向50位的元数据表指针的指针
 metadata_entry_t** meta_table_point = NULL;
+
+printf("2");
 
 //元数据表的初始化函数
 void init_metadata_table(metadata_entry_t **input_meta_table){
