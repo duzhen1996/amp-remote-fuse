40e6aab75ca842b89572ddc9520c45dd3b28a406
ZhenDu <454858191@qq.com>
2018-01-09 19:49:59 +0800

    2018-01-09

--- fuse_client.c
+++ fuse_client.c
@@ -40,7 +40,7 @@ static int amp_readdir(const char* path, void* buf, fuse_fill_dir_t filler,
             printf("pathname:%s,meta->mode:%d", meta_en[i].path_name, meta_en[i].meta->st_mode);
             //后面没有结束符可能会导致严重问题，现在不知道为什么，但是加个`\0`试试
             meta_en[i].path_name[strlen(meta_en[i].path_name)] = '\0';
-            filler(buf, meta_en[i].path_name, meta_en[i].meta ,0);
+            filler(buf, "newtest123", meta_en[i].meta ,0);
         }
     }
 
