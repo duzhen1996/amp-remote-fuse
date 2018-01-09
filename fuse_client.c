e96120e7a825d4c71292e8b15216d84f5c42815e
ZhenDu <454858191@qq.com>
2018-01-09 17:20:53 +0800

    2018-01-09

--- fuse_client.c
+++ fuse_client.c
@@ -40,7 +40,7 @@ static int amp_readdir(const char* path, void* buf, fuse_fill_dir_t filler,
         }
     }
 
-    return filler(buf, "凑数", NULL, 0);
+    // return filler(buf, "凑数", NULL, 0);
 
     return 0;
 }
