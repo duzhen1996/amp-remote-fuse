c71bed7932911ca9489f413e416acd23221ad322
ZhenDu <454858191@qq.com>
2018-01-10 19:50:16 +0800

    2018-01-10

--- fuse_server.c
+++ fuse_server.c
@@ -136,14 +136,17 @@ void server_free_pages (amp_u32_t num, amp_kiov_t **kiov)
 int send_to_client(amp_request_t *req, int result, char* buf){
 	int err = 0;
 	struct stat file_metadata;
-	int buf_size;
+	int buf_size = 0;
 
 	//根据结果修改返回请求的值
 	fuse_msg_t* fusemsg = NULL;
 	fusemsg = (fuse_msg_t *)((char *)req->req_msg + AMP_MESSAGE_HEADER_LEN);
 	file_metadata = fusemsg->server_stat;
 	//段空间可能的大小
-	buf_size = fusemsg->page_size_now;
+	if(fusemsg->type == 1){
+		//如果是读文件的操作，服务器端要准备申请空间了
+		buf_size = fusemsg->bytes;
+	}
 	
 	//先修改消息，返回yes
 	//我觉得这个是一个罪魁祸首的操作，把同样有用的元数据给置0了，此外这里把非常重要的size参数也给清零了
@@ -304,7 +307,7 @@ int slove_request(amp_request_t *req){
 		struct stat meta;
 		memset(&meta, 0, sizeof(struct stat));
 		res = lstat(dest_path, &meta);
-		fusemsg->page_size_now = fusemsg->bytes;
+		// fusemsg->page_size_now = fusemsg->bytes;
 		fusemsg->server_stat = meta;
 		printf("err:%d,mode:%d\n",res,fusemsg->server_stat.st_mode);
 		send_to_client(req,1,read_buf);
